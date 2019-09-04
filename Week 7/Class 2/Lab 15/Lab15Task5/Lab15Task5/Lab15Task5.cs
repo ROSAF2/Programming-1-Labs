bac79bdd88a2e9cd0b13acfc85cc7600d1998f5d
Franz <rosaf2@student.op.ac.nz>
2019-09-04 21:35:01 +1200

    added Lab 15 task 5 and 6

---
+++ Week 7/Class 2/Lab 15/Lab15Task5/Lab15Task5/Lab15Task5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15Task5
+{
+    class Lab15Task5
+    {
+        static void Main()
+        {
+           // char accumulation;
+
+            Console.Write("Please enter a starting letter: ");
+            char start = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("\nPlease now enter an ending letter: ");
+            char end = Convert.ToChar(Console.ReadLine());
+
+            if (start > end) Console.WriteLine("\nPlease make sure they are in order");
+            else Console.WriteLine($"\nfor start value {start} and stop value {end}");
+
+            for (char i = start; i <= end; i++)
+            {
+               // accumulation = start + i;
+                Console.WriteLine(i);
+
+            }
+
+
+            Console.ReadLine();
+                
+        }
+    }
+}
---
+++ Week 7/Class 2/Lab 15/Lab15Task6/Lab15Task6/Lab15Task6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15Task6
+{
+    class Lab15Task6
+    {
+        static void Main()
+        {
+            Random rand = new Random();
+            int va = rand.Next(1, 41);
+            int[] arr = new int[6];
+
+            Console.WriteLine("\t\t\tLotto Problem\n");
+            
+            for (int i = 0; i < 6; i++)
+            {
+                while (va == arr[0] || va == arr[1]|| va == arr[2] || va == arr[3]|| va == arr[4] || va == arr[5])
+                {
+                    va = rand.Next(1, 41);
+                }
+                arr[i] = va;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}.- {arr[i]}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
