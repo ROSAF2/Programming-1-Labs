dc4512b9bc83cd20384747c0236994600b9895d9
Francisco Fernando Rosas Chavez (1000068686) <ROSAF2@student.op.ac.nz>
2019-09-04 12:46:25 +1200

    Undergone massive changes, this is now the base Folder Structure

---
+++ Week 7/Class 1/Lab 14/Lab14Task2/Lab14Task2/Lab14Task2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14Task2
+{
+    class Lab14Task2
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
