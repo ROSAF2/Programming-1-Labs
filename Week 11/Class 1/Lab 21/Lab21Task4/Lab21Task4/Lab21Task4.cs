3e55b7acd28aa1140f88a61d122687c828da2427
Francisco Fernando Rosas Chavez (1000068686) <ROSAF2@student.op.ac.nz>
2019-10-15 12:00:41 +1300

    Week 11 work on lab 21

---
+++ Week 11/Class 1/Lab 21/Lab21Task4/Lab21Task4/Lab21Task4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab21Task4
+{
+    public struct memberDetails
+    {
+        public string firstName;
+        public string lastName;
+        public string phone;
+    }
+    class Lab21Task4
+    {
+        static void Main()
+        {
+            int numberOfMembers = 3;
+            memberDetails[] classMember = new memberDetails[numberOfMembers];
+
+            Console.WriteLine("Enter the class members' information\n");
+
+            for (int i = 0; i < numberOfMembers; i++)
+            {
+                Console.WriteLine("\nMember number {0}",i+1);
+                Console.Write("\nFirst name: ");
+                classMember[i].firstName = Console.ReadLine();
+                Console.Write("\nLast name: ");
+                classMember[i].lastName = Console.ReadLine();
+                Console.Write("\nPhone: ");
+                classMember[i].phone = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < numberOfMembers; i++)
+            {
+                Console.WriteLine("\t\t\t" + classMember[i].firstName.PadRight(15) + classMember[i].lastName.PadRight(15) + classMember[i].phone);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week 11/Class 1/Lab 21/Lab21Task4/Lab21Task4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lab21Task4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab21Task4")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5bab2161-74a9-4e64-ba8a-72e68a1d9792")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
